00f85244b361c27a53d1f93f823e3bab2540c433
Davit Kalantaryan <davit.kalantar@desy.de>
2023-02-27 18:14:53 +0100

    works on windows

--- src/tests/main_insert_custom_info_into_exe_test01.cpp
+++ src/tests/main_insert_custom_info_into_exe_test01.cpp
@@ -5,75 +5,83 @@
 // created by:		Davit Kalantaryan (davit.kalantaryan@desy.de)
 //
 
-
-// pragma_directive_const_seg.cpp
-// compile with: /EHsc
-#include <iostream>
-
-const int i = 7;               // inlined, not stored in .rdata
-const char sz1[] = "test1";     // stored in .rdata
-
-#pragma section(".mydata",read)
-__declspec(allocate(".mydata")) const char sz2[] = "This_is_my_custom_data";     // stored in .my_data1
-
-//#pragma const_seg()
-
-//#pragma const_seg(push, stack1, ".my_data2")
-const char sz3[] = "test3";     // stored in .my_data2
-
-//#pragma const_seg(pop, stack1) // pop stack1 from stack
-const char sz4[] = "test4";     // stored in .my_data1
+//#include <iostream>
 
 
-int main() {
-	using namespace std;
-	// const data must be referenced to be put in .obj
-	cout << sz1 << endl;
-	cout << sz2 << endl;
-	cout << sz3 << endl;
-	cout << sz4 << endl;
-}
-
-
-#if 0
+#ifdef __cplusplus
+#define CPPUTILS_EXTERN_C	extern "C"
+#else
+#define CPPUTILS_EXTERN_C
+#endif
 
 #define STRINGIZER(arg)    #arg
 #define STRVAL(v)          STRINGIZER(v)
+#define CPPUTILS_ID(_x)		_x
 
 #define COMMIT_ID	e6ae7a2e4e7100532b6884d1534f4c8399067413
 
+//#ifdef _MSC_VER
+//#pragma section(".mysect",read)
+//CPPUTILS_EXTERN_C __declspec(allocate(".mysect")) const char commit_id[] = "commit-id=" STRVAL(COMMIT_ID);
+//#pragma comment(linker, "/include:commit_id")
+//#else
+//	const char commit_id[] __attribute__((section("mysection"))) = "commit-id=" STRVAL(COMMIT_ID);
+//#endif
+
+
 #ifdef _MSC_VER
-#pragma data_seg(".mysection")
-const char commit_id[] = "commit-id=" STRVAL(COMMIT_ID);
-#pragma data_seg()
+#define CPPUTILS_INSERT_COMMENT_TO_BIN_RAW_RAW(_sectionVar,_sectionName,_comment)				\
+	__pragma(section(_sectionName,read))														\
+	CPPUTILS_EXTERN_C __declspec(allocate(_sectionName)) const char _sectionVar[] = _comment;	\
+	__pragma(comment(linker, "/include:" STRVAL(_sectionVar)))
 #else
-const char commit_id[] __attribute__((section("mysection"))) = "commit-id=" STRVAL(COMMIT_ID);
+const char _sectionVar[] __attribute__((section(_sectionName))) = _comment;
 #endif
 
-#if 0
-
-#ifdef _MSC_VER
-#define CPPUTILS_INSERT_COMMENT_TO_BIN(_section,_comment)	\
-	__pragma( data_seg(STRVAL(_section)) )					\
-	const char __section_var##_section [] = _comment;		\
-	__pragma( data_seg() )
-#else
-#define CPPUTILS_INSERT_COMMENT_TO_BIN(_section,_comment)	\
-	const char __section_var##_section[] __attribute__((section(STRVAL(_section)))) = _comment;
-#endif
-
-CPPUTILS_INSERT_COMMENT_TO_BIN(mysection, "commit-id=" STRVAL(COMMIT_ID))
+CPPUTILS_INSERT_COMMENT_TO_BIN_RAW_RAW(__section_var1, ".mysect", "commit-id=" STRVAL(COMMIT_ID))
 
 
-#endif
+#define CPPUTILS_NAME_WITH_NUM_RAWEX(_var,_num)		_var ## _nm
+#define CPPUTILS_NAME_WITH_NUM_RAW(_var,_num)		CPPUTILS_NAME_WITH_NUM_RAWEX(_var,_num)
+#define CPPUTILS_NAME_WITH_NUM(_var)				CPPUTILS_NAME_WITH_NUM_RAW(_var,__COUNTER__)
+
+
+//#define CPPUTILS_NAME_WITH_NUM(_num)		__section_var_ ## _num
+
+
+#define CPPUTILS_INSERT_COMMENT_TO_BIN_RAW(_sectionName,_comment)	\
+	CPPUTILS_INSERT_COMMENT_TO_BIN_RAW_RAW(CPPUTILS_NAME_WITH_NUM(___section_var_),_sectionName,_comment)
+
+CPPUTILS_INSERT_COMMENT_TO_BIN_RAW(".mysect", "__commit-id=" STRVAL(COMMIT_ID))
+
+
+//#ifdef _MSC_VER
+//#define CPPUTILS_INSERT_COMMENT_TO_BIN(_section,_comment)	\
+//	__pragma( section(STRVAL(_section),read) )					\
+//	__declspec(allocate(STRVAL(_section))) const char __section_var##_section [] = _comment;
+//#else
+//#define CPPUTILS_INSERT_COMMENT_TO_BIN(_section,_comment)	\
+//	const char __section_var##_section[] __attribute__((section(STRVAL(_section)))) = _comment;
+//#endif
+
+
+//#ifdef _MSC_VER
+//#define CPPUTILS_INSERT_COMMENT_TO_BIN(_sectionVarExt,_sectionName,_comment)		\
+//	__pragma( const_seg( _sectionName) ) 					\
+//	__declspec(allocate( _sectionName )) const char __section_var##_sectionVarExt [] = _comment;	\
+//	__pragma( const_seg() )
+//#else
+//#define CPPUTILS_INSERT_COMMENT_TO_BIN(_section,_comment)	\
+//	const char __section_var##_section[] __attribute__((section(STRVAL(_section)))) = _comment;
+//#endif
+
+
 
 
 int main(int a_argc, char* a_argv[])
 {
+	//(void)commit_id;
 	(void)a_argc;
 	(void)a_argv;
 	return 0;
 }
-
-
-#endif
